95e90f48c9c49490f53198332b131588ee2ddf23
kexamir <122889502+kexamir@users.noreply.github.com>
2023-01-17 17:21:37 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const TelegramBot=require('node-telegram-bot-api');
 const token ="5700381502:AAE0aIizByDmS73mjs_Ywl_9QhFzCGEGqbA";
 const bot = new TelegramBot(token,{polling:true});
  bot.onText(/\/start/,msg=>{
-    //console.log(msg)
+    console.log(msg)
     bot.sendMessage(msg.chat.id,"به ربات تلگرام قطبینو خوش آمدید.")
  }
